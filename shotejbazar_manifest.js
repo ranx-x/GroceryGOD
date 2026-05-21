486b6827ad7eaeef8934efc6b63cd83074acce37
ranehal <ran.ragibahnafnehal2@gmail.com>
2026-05-21 14:42:01 +0600

    fix: successfully restored and merged full multi-month history for all stores (ULTIMATE FINAL)

--- shotejbazar_manifest.js
+++ shotejbazar_manifest.js
@@ -1,1 +1,1 @@
-window.shotejbazarManifest = {"metadata":{"last_update":"2026-05-21 14:26:02","total":461,"date_range":"2026-05-17 to 2026-05-17","total_chunks":1,"chunk_size":20000}};
+window.shotejbazarManifest = {"metadata":{"last_update":"2026-05-21 14:41:57","total":461,"date_range":"2026-05-17 to 2026-05-17","total_chunks":1,"chunk_size":20000}};
