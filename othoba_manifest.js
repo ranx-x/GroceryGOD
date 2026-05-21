a7479f490295f1572d4128b296a3f9eee777623e
ranehal <ran.ragibahnafnehal2@gmail.com>
2026-05-21 16:03:43 +0600

    fix: absolute final bridge for 18-21 May data gap across all stores

--- othoba_manifest.js
+++ othoba_manifest.js
@@ -1,1 +1,1 @@
-window.othobaManifest = {"metadata":{"last_update":"2026-05-21 15:55:07","total":96616,"date_range":"2026-04-17 to 2026-05-21","total_chunks":5,"chunk_size":20000}};
+window.othobaManifest = {"metadata":{"last_update":"2026-05-21 16:03:32","total":96616,"date_range":"2026-04-17 to 2026-05-21","total_chunks":5,"chunk_size":20000}};
