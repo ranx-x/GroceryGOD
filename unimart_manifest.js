580e61059237416891ddb43c2ced087da9571e6f
ranx-x <educational.purpose37@gmail.com>
2026-08-03 15:17:39 +0000

    [Shwapno] Instant Scraper DB Update 2026-08-03 21:17:39

---
+++ unimart_manifest.js
@@ -0,0 +1,1 @@
+window.unimartManifest = {"metadata":{"last_update":"2026-08-03 21:15:49","total":4101,"date_range":"2026-05-18 to 2026-08-03","total_chunks":1,"chunk_size":5000,"scraper_stats":{"web":4101,"app":0,"combined":4101}}};
