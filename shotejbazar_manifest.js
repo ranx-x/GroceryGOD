b14225471bab001989990918f9f02f22ca72e7ac
ranx-x <educational.purpose37@gmail.com>
2026-08-03 15:08:43 +0000

    attempt #8 if this works ill get some sleep frfr: 2026-08-03 21:08:43

--- shotejbazar_manifest.js
+++
@@ -1,1 +0,0 @@
-window.shotejbazarManifest = {"metadata":{"last_update":"2026-08-03 21:00:27","total":632,"date_range":"2026-05-17 to 2026-08-03","total_chunks":1,"chunk_size":5000,"scraper_stats":{"web":632,"app":0,"combined":632}}};
