43c0828ab3eaca44ea8fc9e32c8fb9c3a759db8a
ranx-x <educational.purpose37@gmail.com>
2026-08-03 15:10:05 +0000

    [Shwapno] Instant Scraper DB Update 2026-08-03 21:10:05

--- swapnoTRACKER/shwapno_manifest.js
+++ swapnoTRACKER/shwapno_manifest.js
@@ -3,7 +3,7 @@ window.shwapno_manifest = {
   "app_id": "com.shwapno",
   "api_base": "https://store-api.shwapno.com/en/api",
   "headers": {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/122.0.0.0 Safari/537.36",
     "Accept": "application/json",
     "client-type": "App",
     "customer": "fbbbb451-a780-4101-88e0-aad799e60f83",
@@ -741,5 +741,5 @@ window.shwapno_manifest = {
   ],
   "total_categories": 36,
   "total_products": 3463,
-  "captured_at": "2026-08-03T15:02:20.014861"
+  "captured_at": "2026-08-03T15:09:58.912818"
 };
